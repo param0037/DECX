d787654e2ac85ad0af64a33435869932ac78b5f3
param0037 <93871506+param0037@users.noreply.github.com>
2023-01-12 23:37:58 +0800

    Create IO_GUI.h

---
+++ includes/vision/IO_GUI.h
@@ -0,0 +1,34 @@
+/**
+*	---------------------------------------------------------------------
+*	Author : Wayne Anderson
+*   Date   : 2021.04.16
+*	---------------------------------------------------------------------
+*	This is a part of the open source program named "DECX", copyright c Wayne,
+*	2021.04.16
+*/
+
+
+#ifndef _IO_GUI_H_
+#define _IO_GUI_H_
+
+
+#include "../basic.h"
+#include "../classes/Matrix.h"
+
+
+namespace de {
+	namespace vis {
+		_DECX_API_ void ShowImg(de::Matrix& src, const char* window_name);
+
+
+		_DECX_API_ de::DH ReadImage(const char* img_path, de::Matrix& src);
+
+
+		_DECX_API_ void wait_untill_quit();
+
+
+		_DECX_API_ de::DH merge_channel(de::Matrix& src, de::Matrix& dst, const int flag);
+	}
+}
+
+#endif
