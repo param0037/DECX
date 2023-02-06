2207004a568cdd0f6c6d951ad540afaee342c9c3
param0037 <93871506+param0037@users.noreply.github.com>
2023-02-07 00:39:02 +0800

    Update convolution.h

--- includes/basic_calculations/convolution.h
+++ includes/basic_calculations/convolution.h
@@ -33,42 +33,48 @@ namespace de
 
 	namespace cuda
 	{
-		_DECX_API_ de::DH Conv2(de::Matrix& src, de::Matrix& kernel, de::Matrix& dst, const uint conv_flag, const int accu_flag);
+		_DECX_API_ de::DH Filter2D(de::Matrix& src, de::Matrix& kernel, de::Matrix& dst, const uint conv_flag, const int accu_flag);
 
 
-		_DECX_API_ de::DH Conv2(de::GPU_Matrix& src, de::GPU_Matrix& kernel, de::GPU_Matrix& dst, const uint conv_flag, const int accu_flag);
+		_DECX_API_ de::DH Filter2D(de::GPU_Matrix& src, de::GPU_Matrix& kernel, de::GPU_Matrix& dst, const uint conv_flag, const int accu_flag);
 
 
-		_DECX_API_ de::DH Conv2_single_kernel(
+		_DECX_API_ de::DH Filter2D_single_kernel(
 			de::GPU_MatrixArray& src, de::GPU_Matrix& kernel, de::GPU_MatrixArray& dst, const int conv_flag, const int accu_flag);
 
 
-		_DECX_API_ de::DH Conv2_multi_kernel(
+		_DECX_API_ de::DH Filter2D_multi_kernel(
 			de::GPU_MatrixArray& src, de::GPU_MatrixArray& kernel, de::GPU_MatrixArray& dst, const int conv_flag, const int accu_flag);
 
 
-		_DECX_API_ de::DH Conv2_single_kernel(
+		_DECX_API_ de::DH Filter2D_single_kernel(
 			de::MatrixArray& src, de::Matrix& kernel, de::MatrixArray& dst, const int conv_flag, const int accu_flag);
 
 
-		_DECX_API_ de::DH Conv2_multi_kernel(
+		_DECX_API_ de::DH Filter2D_multi_kernel(
 			de::MatrixArray& src, de::MatrixArray& kernel, de::MatrixArray& dst, const int conv_flag, const int accu_flag);
+
+
+		_DECX_API_ de::DH Conv2D(de::GPU_Tensor& src, de::GPU_TensorArray& kernel,
+			de::GPU_Tensor& dst, const int flag, const int accu_flag);
 	}
 
 
 	namespace cpu
 	{
-		_DECX_API_ de::DH Conv2(de::Matrix& src, de::Matrix& kernel, de::Matrix& dst, const int flag, const int _output_type);
-
-
-		//_DECX_API_ de::DH Conv2(de::Matrix& src, de::Matrix& kernel, de::Matrix& dst, const int flag);
-
-
-		_DECX_API_ de::DH Conv2_single_channel(de::MatrixArray& src, de::Matrix& kernel, de::MatrixArray& dst, const int flag);
+		_DECX_API_ de::DH Filter2D(de::Matrix& src, de::Matrix& kernel, de::Matrix& dst, const int flag, const int _output_type);
 
 
 
-		_DECX_API_ de::DH Conv2_multi_channel(de::MatrixArray& src, de::MatrixArray& kernel, de::MatrixArray& dst, const int flag);
+		_DECX_API_ de::DH Filter2D_single_channel(de::MatrixArray& src, de::Matrix& kernel, de::MatrixArray& dst, const int flag);
+
+
+
+		_DECX_API_ de::DH Filter2D_multi_channel(de::MatrixArray& src, de::MatrixArray& kernel, de::MatrixArray& dst, const int flag);
+
+
+
+		_DECX_API_ de::DH Conv2D(de::Tensor& src, de::TensorArray& kernel, de::Tensor& dst, const de::Point2D strideXY, const int conv_flag);
 	}
 }
 
