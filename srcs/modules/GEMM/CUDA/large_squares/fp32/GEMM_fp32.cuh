6ae3654d372bbdfae380f0d7628d2e408d31703b
param0037 <93871506+param0037@users.noreply.github.com>
2023-02-13 17:49:16 +0800

    Update GEMM_fp32.cuh

--- srcs/modules/GEMM/CUDA/large_squares/fp32/GEMM_fp32.cuh
+++ srcs/modules/GEMM/CUDA/large_squares/fp32/GEMM_fp32.cuh
@@ -17,179 +17,136 @@
 
 
 // last storage (16, 16)
-// ¼ÆËã / ·Ã´æ ±È is the crucial, reduce memory assess by vectorization
-__global__
-/**
-* config -> <<<dim3(h / 128, w / 128, 1), int(16 * 16), 0, S>>>
-* __same should be 16-times and dstDims should be both 128-times
-* @param pitch_A : considered float4, is the true width on device memory (>= ~.width)
-* @param pitch_B : considered float4, is the true width on device memory (>= ~.width)
-* @param pitch_dst : considered float4
-* @param __iter : __linear(in float) / 16
-*/
-void cu_GEMM_fp32_spec(float4* A,
-    float4* B,
-    float4* dst,
-    const uint                pitch_A,
-    const uint                pitch_B,
-    const uint                __iter);
+// è®¡ç®— / è®¿å­˜ æ¯” is the crucial, reduce memory assess by vectorization
+
+namespace decx {
+    namespace gemm {
+        namespace GPUK {
+
+            __global__
+                /**
+                * config -> <<<dim3(h / 128, w / 128, 1), int(16 * 16), 0, S>>>
+                * __same should be 16-times and dstDims should be both 128-times
+                * @param pitch_A : considered float4, is the true width on device memory (>= ~.width)
+                * @param pitch_B : considered float4, is the true width on device memory (>= ~.width)
+                * @param pitch_dst : considered float4
+                * @param __iter : __linear(in float) / 16
+                */
+                void cu_GEMM_fp32_spec(float4* A, float4* B, float4* dst,
+                    const uint pitch_A, const uint pitch_B, const uint __iter);
 
 
 
 
-// last storage (16, 16)
-// ¼ÆËã / ·Ã´æ ±È is the crucial, reduce memory assess by vectorization
-__global__
-/**
-* config -> <<<dim3(h / 128, w / 128, 1), int(16 * 16), 0, S>>>
-* __same should be 16-times and dstDims should be both 128-times
-* @param pitch_A : considered float4, is the true width on device memory (>= ~.width)
-* @param pitch_B : considered float4, is the true width on device memory (>= ~.width)
-* @param pitch_dst : considered float4
-* @param __iter : __linear(in float) / 16
-*/
-void cu_GEMM_fp32_ABC_spec(float4* A,
-    float4* B,
-    float4* C,
-    float4* dst,
-    const uint              pitch_A,
-    const uint              pitch_B,
-    const uint              __iter);
+
+            __global__
+                /**
+                * config -> <<<dim3(h / 128, w / 128, 1), int(16 * 16), 0, S>>>
+                * __same should be 16-times and dstDims should be both 128-times
+                * @param pitch_A : considered float4, is the true width on device memory (>= ~.width)
+                * @param pitch_B : considered float4, is the true width on device memory (>= ~.width)
+                * @param pitch_dst : considered float4
+                * @param __iter : __linear(in float) / 16
+                */
+                void cu_GEMM_fp32_ABC_spec(float4* A, float4* B, float4* C, float4* dst,
+                    const uint pitch_A, const uint pitch_B, const uint __iter);
 
 
 
 
-// last storage (16, 16)
-// ¼ÆËã / ·Ã´æ ±È is the crucial, reduce memory assess by vectorization
-__global__
-/**
-* config -> <<<dim3(h / 128, w / 128, 1), int(16 * 16), 0, S>>>
-* __same should be 16-times and dstDims should be both 128-times
-* @param pitch_A : considered float4, is the true width on device memory (>= ~.width)
-* @param pitch_B : considered float4, is the true width on device memory (>= ~.width)
-* @param pitch_dst : considered float4
-* @param bounds : ~.x : width (in float4); ~.y : height
-* @param __iter : __linear(in float) / 16
-*/
-void cu_GEMM_fp32_anyWH_specL(float4* A,
-    float4* B,
-    float4* dst,
-    const uint                pitch_A,
-    const uint                pitch_B,
-    const uint                Hdst,
-    const uint                __iter);
 
-
-
-// last storage (16, 16)
-// ¼ÆËã / ·Ã´æ ±È is the crucial, reduce memory assess by vectorization
-__global__
-/**
-* config -> <<<dim3(h / 128, w / 128, 1), int(16 * 16), 0, S>>>
-* __same should be 16-times and dstDims should be both 128-times
-* @param pitch_A : considered float4, is the true width on device memory (>= ~.width)
-* @param pitch_dst : considered float4
-* @param HB : lenght of linear region(_B.height) (in float)
-* @param __iter : __linear(in float) / 16
-*/
-void cu_GEMM_fp32_ABC_anyWH_specL(float4* A,
-    float4* B,
-    float4* C,
-    float4* dst,
-    const uint                pitch_A,
-    const uint                pitch_B,
-    const uint                Hdst,
-    const uint                __iter);
+            __global__
+                /**
+                * config -> <<<dim3(h / 128, w / 128, 1), int(16 * 16), 0, S>>>
+                * __same should be 16-times and dstDims should be both 128-times
+                * @param pitch_A : considered float4, is the true width on device memory (>= ~.width)
+                * @param pitch_B : considered float4, is the true width on device memory (>= ~.width)
+                * @param pitch_dst : considered float4
+                * @param bounds : ~.x : width (in float4); ~.y : height
+                * @param __iter : __linear(in float) / 16
+                */
+                void cu_GEMM_fp32_anyWH_specL(float4* A, float4* B, float4* dst,
+                    const uint pitch_A, const uint pitch_B, const uint Hdst, const uint __iter);
 
 
 
 
-// last storage (16, 16)
-// ¼ÆËã / ·Ã´æ ±È is the crucial, reduce memory assess by vectorization
-__global__
-/**
-* config -> <<<dim3(h / 128, w / 128, 1), int(16 * 16), 0, S>>>
-* __same should be 16-times and dstDims should be both 128-times
-* @param pitch_A : considered float4, is the true width on device memory (>= ~.width)
-* @param pitch_dst : considered float4
-* @param HB : lenght of linear region(_B.height) (in float)
-* @param __iter : __linear(in float) / 16
-*/
-void cu_GEMM_fp32_anyWH_anyL(float4* A,
-    float4* B,
-    float4* dst,
-    const uint                pitch_A,
-    const uint                pitch_B,
-    const uint                Hdst,
-    const uint                HB,
-    const uint                __iter);
+            __global__
+                /**
+                * config -> <<<dim3(h / 128, w / 128, 1), int(16 * 16), 0, S>>>
+                * __same should be 16-times and dstDims should be both 128-times
+                * @param pitch_A : considered float4, is the true width on device memory (>= ~.width)
+                * @param pitch_dst : considered float4
+                * @param HB : lenght of linear region(_B.height) (in float)
+                * @param __iter : __linear(in float) / 16
+                */
+                void cu_GEMM_fp32_ABC_anyWH_specL(float4* A, float4* B, float4* C, float4* dst,
+                    const uint pitch_A, const uint pitch_B, const uint Hdst, const uint __iter);
 
 
 
-// last storage (16, 16)
-// ¼ÆËã / ·Ã´æ ±È is the crucial, reduce memory assess by vectorization
-__global__
-/**
-* config -> <<<dim3(h / 128, w / 128, 1), int(16 * 16), 0, S>>>
-* __same should be 16-times and dstDims should be both 128-times
-* @param pitch_A : considered float4, is the true width on device memory (>= ~.width)
-* @param pitch_dst : considered float4
-* @param HB : lenght of linear region(_B.height) (in float)
-* @param __iter : __linear(in float) / 16
-*/
-void cu_GEMM_fp32_ABC_anyWH_anyL(float4* A,
-    float4* B,
-    float4* C,
-    float4* dst,
-    const uint                pitch_A,
-    const uint                pitch_B,
-    const uint                Hdst,
-    const uint                HB,
-    const uint                __iter);
+
+
+            __global__
+                /**
+                * config -> <<<dim3(h / 128, w / 128, 1), int(16 * 16), 0, S>>>
+                * __same should be 16-times and dstDims should be both 128-times
+                * @param pitch_A : considered float4, is the true width on device memory (>= ~.width)
+                * @param pitch_dst : considered float4
+                * @param HB : lenght of linear region(_B.height) (in float)
+                * @param __iter : __linear(in float) / 16
+                */
+                void cu_GEMM_fp32_anyWH_anyL(float4* A, float4* B, float4* dst,
+                    const uint pitch_A, const uint pitch_B, const uint Hdst, const uint HB, const uint __iter);
 
 
 
-// last storage (16, 16)
-// ¼ÆËã / ·Ã´æ ±È is the crucial, reduce memory assess by vectorization
-__global__
-/**
-* config -> <<<dim3(h / 128, w / 128, 1), int(16 * 16), 0, S>>>
-* __same should be 16-times and dstDims should be both 128-times
-* @param pitch_A : considered float4, is the true width on device memory (>= ~.width)
-* @param pitch_dst : considered float4
-* @param HB : lenght of linear region(_B.height) (in float)
-* @param __iter : __linear(in float) / 16
-*/
-void cu_GEMM_fp32_specWH_anyL(float4* A,
-    float4* B,
-    float4* dst,
-    const uint                pitch_A,
-    const uint                pitch_B,
-    const uint                HB,
-    const uint                __iter);
+
+            __global__
+                /**
+                * config -> <<<dim3(h / 128, w / 128, 1), int(16 * 16), 0, S>>>
+                * __same should be 16-times and dstDims should be both 128-times
+                * @param pitch_A : considered float4, is the true width on device memory (>= ~.width)
+                * @param pitch_dst : considered float4
+                * @param HB : lenght of linear region(_B.height) (in float)
+                * @param __iter : __linear(in float) / 16
+                */
+                void cu_GEMM_fp32_ABC_anyWH_anyL(float4* A, float4* B, float4* C, float4* dst,
+                    const uint pitch_A, const uint pitch_B, const uint Hdst, const uint HB, const uint __iter);
 
 
 
-// last storage (16, 16)
-// ¼ÆËã / ·Ã´æ ±È is the crucial, reduce memory assess by vectorization
-__global__
-/**
-* config -> <<<dim3(h / 128, w / 128, 1), int(16 * 16), 0, S>>>
-* __same should be 16-times and dstDims should be both 128-times
-* @param pitch_A : considered float4, is the true width on device memory (>= ~.width)
-* @param pitch_dst : considered float4
-* @param HB : lenght of linear region(_B.height) (in float)
-* @param __iter : __linear(in float) / 16
-*/
-void cu_GEMM_fp32_ABC_specWH_anyL(float4* A,
-    float4* B,
-    float4* C,
-    float4* dst,
-    const uint                pitch_A,
-    const uint                pitch_B,
-    const uint                HB,
-    const uint                __iter);
 
+            __global__
+                /**
+                * config -> <<<dim3(h / 128, w / 128, 1), int(16 * 16), 0, S>>>
+                * __same should be 16-times and dstDims should be both 128-times
+                * @param pitch_A : considered float4, is the true width on device memory (>= ~.width)
+                * @param pitch_dst : considered float4
+                * @param HB : lenght of linear region(_B.height) (in float)
+                * @param __iter : __linear(in float) / 16
+                */
+                void cu_GEMM_fp32_specWH_anyL(float4* A, float4* B, float4* dst,
+                    const uint pitch_A, const uint pitch_B, const uint HB, const uint __iter);
+
+
+
+
+            __global__
+                /**
+                * config -> <<<dim3(h / 128, w / 128, 1), int(16 * 16), 0, S>>>
+                * __same should be 16-times and dstDims should be both 128-times
+                * @param pitch_A : considered float4, is the true width on device memory (>= ~.width)
+                * @param pitch_dst : considered float4
+                * @param HB : lenght of linear region(_B.height) (in float)
+                * @param __iter : __linear(in float) / 16
+                */
+                void cu_GEMM_fp32_ABC_specWH_anyL(float4* A, float4* B, float4* C, float4* dst,
+                    const uint pitch_A, const uint pitch_B, const uint HB, const uint __iter);
+
+        }
+    }
+}
 
 
 #endif
