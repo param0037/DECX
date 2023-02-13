2e5c1bddeba3064cb05dc11eb54d48f228f2841a
param0037 <93871506+param0037@users.noreply.github.com>
2023-02-13 17:42:50 +0800

    Add files via upload

---
+++ srcs/modules/GEMM/CPU/64b/GEMM_64b_defines.h
@@ -0,0 +1,20 @@
+/**
+*   ---------------------------------------------------------------------
+*   Author : Wayne Anderson
+*   Date   : 2021.04.16
+*   ---------------------------------------------------------------------
+*   This is a part of the open source program named "DECX", copyright c Wayne,
+*   2021.04.16, all right reserved.
+*   More information please visit https://github.com/param0037/backup_1
+*/
+
+#ifndef _GEMM_64B_DEFINES_H_
+#define _GEMM_64B_DEFINES_H_
+
+
+#define _BLOCKED_GEMM_FP64_HA_ 16
+#define _BLOCKED_GEMM_FP64_WB_ 4
+#define _BLOCKED_GEMM_FP64_LINEAR_ 8
+
+
+#endif
